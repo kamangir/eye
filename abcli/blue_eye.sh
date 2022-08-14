49e07e3498136342c9bc061fa83e88cc19ca3ca3
kamangir <arash@kamangir.net>
2022-08-14 16:12:53 -0700

    working on blue_eye start_session - kamangir/bolt#697

--- abcli/blue_eye.sh
+++ abcli/blue_eye.sh
@@ -8,8 +8,8 @@ function blue_eye() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_help_line "blue_eye task_1" \
-            "run blue_eye task_1."
+        abcli_help_line "blue_eye start_session" \
+            "start a blue_eye session."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m blue_eye --help
@@ -20,13 +20,6 @@ function blue_eye() {
 
     if [[ $(type -t blue_eye_$task) == "function" ]] ; then
         blue_eye_$task ${@:2}
-    fi
-
-    if [ "$task" == "task_1" ] ; then
-        python3 -m blue_eye \
-            task_1 \
-            ${@:2}
-        return
     fi
 
     abcli_log_error "-blue_eye: $task: command not found."
---
+++ abcli/session.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+function blue_eye_session() {
+    bolt_log "blue-eye: session: work in progress..."
+}
