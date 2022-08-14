4405657a402df9b5ac054edd0016456ace8da03e
kamangir <arash@kamangir.net>
2022-08-14 16:20:10 -0700

    working on blue_eye start_session - kamangir/bolt#697

--- abcli/blue_eye.sh
+++ abcli/blue_eye.sh
@@ -20,6 +20,7 @@ function blue_eye() {
 
     if [[ $(type -t blue_eye_$task) == "function" ]] ; then
         blue_eye_$task ${@:2}
+        return
     fi
 
     abcli_log_error "-blue_eye: $task: command not found."
--- blue_eye/__init__.py
+++ blue_eye/__init__.py
@@ -1,5 +1,5 @@
 name = "blue_eye"
 
-version = "1.29.1"
+version = "1.31.1"
 
 description = "an abcli plugin for a cloud-connected raspberry pi/jetson camera"
