6772d4c3199f9f585f1d33c948dd0c02343d9f2f
kamangir <arash@kamangir.net>
2022-08-20 11:01:02 -0700

    COOKIE -> cookie - kamangir/bolt#697

--- blue_eye/session.py
+++ blue_eye/session.py
@@ -1,5 +1,5 @@
 from abcli.modules.hardware import instance as hardware
-from abcli.modules.host.session import COOKIE
+from abcli.modules.host import cookie
 from abcli.modules.host.session import Session
 from abcli.plugins.storage import instance as storage
 from abcli.modules import terraform
@@ -21,10 +21,10 @@ class Blue_Eye_Session(Session):
         }.items():
             self.add_timer(name, period)
 
-        self.auto_upload = COOKIE.get("host.session.auto_upload", True)
-        self.outbound_queue = COOKIE.get("host.session.outbound_queue", "stream")
-        self.do_annotate = COOKIE.get("host.session.capture.annotate", True)
-        self.capture_enabled = COOKIE.get("host.session.capture.enabled", True)
+        self.auto_upload = cookie.get("host.session.auto_upload", True)
+        self.outbound_queue = cookie.get("host.session.outbound_queue", "stream")
+        self.do_annotate = cookie.get("host.session.capture.annotate", True)
+        self.capture_enabled = cookie.get("host.session.capture.enabled", True)
 
     def check_camera(self):
         self.new_frame = False
