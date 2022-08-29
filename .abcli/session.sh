150c55b66083eaeff407cc4a7bf8440ee14e219a
kamangir <arash@kamangir.net>
2022-08-28 21:15:10 -0700

    validating blue-sbc - kamangir/bolt#699

--- .abcli/session.sh
+++ .abcli/session.sh
@@ -4,7 +4,7 @@ function blue_sbc_session() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_help_line "blue_sbc session start [--output screen]" \
+        abcli_help_line "blue_sbc session start [output=screen]" \
             "start a blue_sbc session."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
@@ -19,9 +19,24 @@ function blue_sbc_session() {
 
         abcli_tag set $abcli_object_name session,$abcli_host_name,$(abcli_string_today),$abcli_fullname,open,$abcli_wifi_ssid
 
+        local options="$3"
+        local output=""
+        if [[ "$abcli_is_mac" == true ]] ; then
+            local output="screen"
+        elif [[ "$abcli_is_rpi" == true ]] && [[ "$abcli_is_headless" == false ]] ; then
+            local output="screen"
+        fi
+        local output=$(abcli_option "$options" "output" $output)
+
+        local extra_args=""
+        if [ ! -z "$output" ] ; then
+            local extra_args="--output $output"
+        fi
+
         python3 -m blue_sbc.session \
             start \
-            ${@:3}
+            $extra_args \
+            ${@:4}
 
         abcli_upload open
 
