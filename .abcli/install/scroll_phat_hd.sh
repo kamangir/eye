ca45cc9bf4e3006cf1d246d2faf263a3bb54b926
kamangir <arash@kamangir.net>
2022-09-05 17:41:36 -0700

    += scroll_phat_hd - kamangir/bolt#703

--- .abcli/install/scroll_phat_hd.sh
+++ .abcli/install/scroll_phat_hd.sh
@@ -4,6 +4,9 @@ function abcli_install_scroll_phat_hd() {
     pushd $abcli_path_home/git > /dev/null
     git clone https://github.com/pimoroni/scroll-phat-hd
     popd > /dev/null
+
+    # https://github.com/pimoroni/scroll-phat-hd
+    sudo apt-get install python3-scrollphathd
 }
 
 if [ "$(abcli cookie read session.screen display)" == "scroll_phat_hd" ] ; then
