5d6197f5d5497c8e0bd4945653df19ca4aa11a25
kamangir <arash@kamangir.net>
2022-11-16 20:14:56 -0800

    += grove - kamangir/bolt#731

--- .abcli/install/grove.sh
+++ .abcli/install/grove.sh
@@ -2,14 +2,15 @@
 
 function abcli_install_grove() {
     pushd $abcli_path_git > /dev/null
+
     # https://wiki.seeedstudio.com/Grove_Base_Kit_for_Raspberry_Pi/
     curl https://github.com/Seeed-Studio/grove.py/raw/master/install.sh \
         --output grove_install.sh
-    #  | sudo bash -s -
+    sudo bash ./grove_install.sh
 
     popd > /dev/null
 }
 
 if [ "$(abcli cookie read hat.type else)" == "grove" ] ; then
-    abcli_install_module grove 104
+    abcli_install_module grove 105
 fi
--- blue_sbc/__init__.py
+++ blue_sbc/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_sbc"
 
-VERSION = "5.506.1"
+VERSION = "5.507.1"
