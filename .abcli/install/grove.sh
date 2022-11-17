828c1675088a3a7f66fc488bf53eaa3077ba6ada
kamangir <arash@kamangir.net>
2022-11-16 20:10:58 -0800

    += grove - kamangir/bolt#731

--- .abcli/install/grove.sh
+++ .abcli/install/grove.sh
@@ -3,7 +3,8 @@
 function abcli_install_grove() {
     pushd $abcli_path_git > /dev/null
     # https://wiki.seeedstudio.com/Grove_Base_Kit_for_Raspberry_Pi/
-    curl -sL https://github.com/Seeed-Studio/grove.py/raw/master/install.sh
+    curl https://github.com/Seeed-Studio/grove.py/raw/master/install.sh \
+        --output grove_install.sh
     #  | sudo bash -s -
 
     popd > /dev/nul
