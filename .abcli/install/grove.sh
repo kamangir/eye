642d259b3e1e608a701d6ea7c46fe3462e1fc16b
kamangir <arash@kamangir.net>
2022-11-16 20:06:40 -0800

    += grove - kamangir/bolt#731

--- .abcli/install/grove.sh
+++ .abcli/install/grove.sh
@@ -4,6 +4,6 @@ function abcli_install_grove() {
     abcli_log "Installing..."
 }
 
-if [ "$(abcli cookie read session.screen display)" == "grove" ] ; then
-    abcli_install_module grove 101
+if [ "$(abcli cookie read hat.type else)" == "grove" ] ; then
+    abcli_install_module grove 102
 fi
--- blue_sbc/__init__.py
+++ blue_sbc/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_sbc"
 
-VERSION = "5.496.1"
+VERSION = "5.497.1"
