8809df39d8338dea7c13ab4fa639da2a4758c53f
kamangir <arash@kamangir.net>
2022-11-16 20:22:39 -0800

    += grove - kamangir/bolt#731

--- .abcli/install/grove.sh
+++ .abcli/install/grove.sh
@@ -4,7 +4,7 @@ function abcli_install_grove() {
     pushd $abcli_path_git > /dev/null
 
     # https://wiki.seeedstudio.com/Grove_Base_Kit_for_Raspberry_Pi/
-    curl https://github.com/Seeed-Studio/grove.py/raw/master/install.sh \
+    curl -L https://github.com/Seeed-Studio/grove.py/raw/master/install.sh \
         --output grove_install.sh
     sudo bash ./grove_install.sh
 
--- blue_sbc/__init__.py
+++ blue_sbc/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_sbc"
 
-VERSION = "5.507.1"
+VERSION = "5.508.1"
