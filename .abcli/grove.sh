517c5b9df505272f4875c85f18c6a66f143e1a0e
kamangir <arash@kamangir.net>
2022-11-26 20:47:18 -0800

    += grove - kamangir/bolt#731

--- .abcli/grove.sh
+++ .abcli/grove.sh
@@ -10,8 +10,10 @@ function blue_sbc_grove() {
     if [ $task == "help" ] ; then
         abcli_show_usage "grove info" \
             "show grove info."
-        abcli_show_usage "grove validate [adc|button|oled_128x64]" \
-            "validate grove."
+        abcli_show_usage "grove validate [adc|button]" \
+            "validate grove adc | button."
+        abcli_show_usage "grove validate oled_128x64$ABCUL[animate|buttons|image|shapes|stats]" \
+            "validate grove oled_128x64."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m blue_sbc.hat --help
@@ -32,23 +34,25 @@ function blue_sbc_grove() {
         local args=""
         local filepath="grove.py/grove"
         if [ "$what" == "adc" ]; then
-            local filename="adc.py"
+            local filename="adc"
         elif [ "$what" == "button" ]; then
-            local filename="grove_button.py"
+            local filename="grove_button"
             local args="24"
         elif [ "$what" == "oled_128x64" ]; then
             local filepath="Seeed_Python_SSD1315/examples"
-            local filename="image.py"
+            local filename="image"
         else
             abcli_log_error "- blue-sbc: grove: $task: $what: hardware not found."
             return
         fi
 
+        local filename=$(abcli_clarify_input $3 $filename)
+
         local grove_path=$abcli_path_git/$filepath
 
-        abcli_log "validating grove $what: $grove_path/$filename $args"
+        abcli_log "validating grove $what: $grove_path/$filename.py $args"
         pushd $grove_path > /dev/null
-        python3 $filename $args
+        python3 $filename.py $args
         popd > /dev/null
 
         return
