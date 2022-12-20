8dbb6c0a6d092c1696c0766a6b11aba70167557f
kamangir <arash@kamangir.net>
2022-12-19 21:25:22 -0800

    vaidating sparkfun_top_phat leds - kamangir/bolt#738

--- .abcli/sparkfun_top_phat.sh
+++ .abcli/sparkfun_top_phat.sh
@@ -4,15 +4,22 @@ function blue_sbc_sparkfun_top_phat() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_show_usage "blue_sbc sparkfun_top_phat validate" \
+        abcli_show_usage "blue_sbc sparkfun_top_phat validate [button|led]" \
             "validate sparkfun_top_phat."
         return
     fi
 
     if [ "$task" == "validate" ] ; then
-        pushd $abcli_path_git/Top_pHAT_Button_Py/examples > /dev/null
-        python3 top_phat_button_ex2.py
-        popd > /dev/null
+        local what=$(abcli_clarify_input $2 button)
+
+        if [ "$what" == button ] ; then
+            pushd $abcli_path_git/Top_pHAT_Button_Py/examples > /dev/null
+            python3 top_phat_button_ex2.py
+            popd > /dev/null
+            return
+        fi
+
+        abcli_log_error "- blue-sbc: sparkfun_top_phat: $task: $what: hardware not found."
         return
     fi
 
--- blue_sbc/__init__.py
+++ blue_sbc/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_sbc"
 
-VERSION = "5.591.1"
+VERSION = "5.592.1"
