06a8736926e2a1a503fd954fe44bb71a49a4de53
kamangir <arash@kamangir.net>
2022-12-19 20:55:38 -0800

    validating Top_pHAT_Button_Py - kamangir/bolt#738

--- .abcli/install/sparkfun_qwiic.sh
+++ .abcli/install/sparkfun_qwiic.sh
@@ -3,6 +3,10 @@
 function abcli_install_sparkfun_qwiic() {
     # https://learn.sparkfun.com/tutorials/sparkfun-top-phat-hookup-guide/button-controller
     sudo pip3 install sparkfun-qwiic
+
+    pushd $abcli_path_home/git > /dev/null
+    git clone https://github.com/sparkfun/Top_pHAT_Button_Py
+    popd > /dev/null
 }
 
 if [ "$(abcli cookie read hat.kind else)" == "sparkfun-top-phat" ] ; then
--- blue_sbc/__init__.py
+++ blue_sbc/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_sbc"
 
-VERSION = "5.584.1"
+VERSION = "5.585.1"
