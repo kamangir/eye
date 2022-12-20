4aa9a27fed1f84e4781877574981edceab139d62
kamangir <arash@kamangir.net>
2022-12-19 21:01:27 -0800

    validating sparkfun_qwiic leds - kamangir/bolt#738

--- .abcli/install/sparkfun_qwiic.sh
+++ .abcli/install/sparkfun_qwiic.sh
@@ -4,6 +4,9 @@ function abcli_install_sparkfun_qwiic() {
     # https://learn.sparkfun.com/tutorials/sparkfun-top-phat-hookup-guide/button-controller
     sudo pip3 install sparkfun-qwiic
 
+    # https://learn.sparkfun.com/tutorials/sparkfun-top-phat-hookup-guide/ws2812b-leds
+    sudo pip3 install adafruit-circuitpython-neopixel
+
     pushd $abcli_path_home/git > /dev/null
     git clone https://github.com/sparkfun/Top_pHAT_Button_Py
     popd > /dev/null
