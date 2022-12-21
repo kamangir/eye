8cd06289d5f5854f2cf5427d0f99bc843d5571e3
kamangir <arash@kamangir.net>
2022-12-20 21:02:50 -0800

    refactor - kamangir/bolt#738

--- blue_sbc/__init__.py
+++ blue_sbc/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_sbc"
 
-VERSION = "5.595.1"
+VERSION = "5.596.1"
--- blue_sbc/screen/__init__.py
+++ blue_sbc/screen/__init__.py
@@ -8,15 +8,14 @@ logger = logging.getLogger(__name__)
 
 NAME = "blue_sbc.screen"
 
-hat_kind = cookie.get("hat.kind", "else")
-screen_name = cookie.get("session.screen", "display")
-if screen_name == "adafruit_rgb_matrix":
+hat_kind = cookie.get("hat.kind", "other")
+if hat_kind == "adafruit_rgb_matrix":
     from .adafruit_rgb_matrix import instance as screen
-elif screen_name == "scroll_phat_hd":
+elif hat_kind == "scroll_phat_hd":
     from .scroll_phat_hd import instance as screen
-elif screen_name == "template":
+elif hat_kind == "template":
     from .template import instance as screen
-elif screen_name == "unicorn_16x16":
+elif hat_kind == "unicorn_16x16":
     from .unicorn_16x16 import instance as screen
 elif hat_kind == "grove":
     from .grove import instance as screen
