8991a2128a52a68daa54fd708bdc1b845e7b20f3
kamangir <arash@kamangir.net>
2022-12-23 21:06:12 -0800

    hardware refactor - kamangir/bolt#738

--- .abcli/session.sh
+++ .abcli/session.sh
@@ -4,7 +4,7 @@ function blue_sbc_session() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_show_usage "blue_sbc session start$ABCUL[<options>]$ABCUL[--application <application>]" \
+        abcli_show_usage "blue_sbc session start$ABCUL[app=<application>]$ABCUL[<args>]" \
             "start a blue_sbc session."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
@@ -15,16 +15,23 @@ function blue_sbc_session() {
     fi
 
     if [ "$task" == "start" ] ; then
-        abcli_log "blue-sbc: session started."
+        local options=$2
+        local app_name=$(fast_option "$options" app)
+
+        abcli_log "blue-sbc: session started $options"
 
         abcli_tag set \
             $abcli_object_name \
-            open,session,$abcli_hostname,$(abcli_string_today),$(abcli_cookie read session.object.tags)
+            open,session,$abcli_hostname,$(abcli_string_today),$(abcli_cookie read session.object.tags),$app_name
 
-        local options="$2"
+        local extra_args=""
+        if [ ! -z "$app_name" ] ; then
+            local extra_args="--application $app_name"
+        fi
 
         python3 -m blue_sbc.session \
             start \
+            $extra_args \
             ${@:3}
 
         abcli_upload open
