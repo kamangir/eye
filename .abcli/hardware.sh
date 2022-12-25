9cf046066ab4cff58eb10b290212ee718bf64bb4
kamangir <arash@kamangir.net>
2022-12-24 18:47:03 -0800

    hardware refactor - kamangir/bolt#738

--- .abcli/hardware.sh
+++ .abcli/hardware.sh
@@ -1,32 +1,32 @@
 #! /usr/bin/env bash
 
-function blue_sbc_hat() {
+function blue_sbc_hardware() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ "$task" == "help" ] ; then
         abcli_show_usage "blue_sbc Hardware input" \
             "read Hardware inputs."
         abcli_show_usage "blue_sbc Hardware output <10101010>" \
-            "activate hat outputs to 10101010."
-        abcli_show_usage "blue_sbc hat validate" \
-            "validate hat."
+            "activate hardware outputs to 10101010."
+        abcli_show_usage "blue_sbc hardware validate" \
+            "validate hardware."
 
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
-            python3 -m blue_sbc.hat --help
+            python3 -m blue_sbc.hardware --help
         fi
         return
     fi
 
     if [ "$task" == "input" ] ; then
-        python3 -m blue_sbc.hat \
+        python3 -m blue_sbc.hardware \
             input \
             ${@:2}
         return
     fi
 
     if [ "$task" == "output" ] ; then
-        python3 -m blue_sbc.hat \
+        python3 -m blue_sbc.hardware \
             output \
             --outputs "$2" \
             ${@:3}
@@ -34,11 +34,11 @@ function blue_sbc_hat() {
     fi
 
     if [ "$task" == "validate" ] ; then
-        python3 -m blue_sbc.hat \
+        python3 -m blue_sbc.hardware \
             validate \
             ${@:2}
         return
     fi
 
-    abcli_log_error "-blue_sbc: hat: $task: command not found."
+    abcli_log_error "-blue_sbc: hardware: $task: command not found."
 }
