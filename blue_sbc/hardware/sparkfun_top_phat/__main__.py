927b1dc13a4324443b9a315a25cad2336e746492
kamangir <arash@kamangir.net>
2022-12-27 18:43:34 -0800

    validating sparkfun_top_phat - kamangir/bolt#738

--- blue_sbc/hardware/sparkfun_top_phat/__main__.py
+++ blue_sbc/hardware/sparkfun_top_phat/__main__.py
@@ -2,7 +2,7 @@ import argparse
 import time
 from . import *
 from .classes import Sparkfun_Top_phat
-from abcli import string
+from matplotlib import cm
 from abcli import logging
 import logging
 
@@ -22,12 +22,23 @@ hardware = Sparkfun_Top_phat()
 
 success = False
 if args.task == "validate_leds":
+    # https://matplotlib.org/3.1.1/tutorials/colors/colormap-manipulation.html
+    # https://matplotlib.org/stable/tutorials/colors/colormaps.html
+
+    colormap = cm.get_cmap("GnBu", hardware.pixel_count)(range(hardware.pixel_count))
+
     logger.info("loop started (Ctrl+C to stop)")
+    offset = 0
     # https://stackoverflow.com/a/18994932/10917551
     try:
         while True:
             for index in range(hardware.pixel_count):
-                hardware.pixels[index] = (10, 0, 0)
+                hardware.pixels[index] = tuple(
+                    int(thing * 255)
+                    for thing in colormap[(index + offset) % hardware.pixel_count][:3]
+                )
+
+            offset += 1
 
             hardware.pixels.show()
             time.sleep(0.1)
