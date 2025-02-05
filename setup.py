70dc9d6d802dae1ac485ece9b2942482d80e93da
kamangir <arash@kamangir.net>
2025-02-05 13:19:26 -0800

    start of restart-2025-02-05-keqs5v 🪄 - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
-from setuptools import setup
+from blueness.pypi import setup
 
-from blue_sbc import NAME, VERSION
+from blue_sbc import NAME, VERSION, DESCRIPTION, REPO_NAME
+
 
 setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
     name=NAME,
-    author="kamangir",
     version=VERSION,
-    description="an abcli plugin for cloud-connected raspberry pi/jetson hardware",
-    packages=[NAME],
+    description=DESCRIPTION,
+    packages=[
+        NAME,
+        f"{NAME}.algo",
+        f"{NAME}.applications",
+        f"{NAME}.hardware",
+        f"{NAME}.hardware.hat",
+        f"{NAME}.hardware.sparkfun_top_phat",
+        f"{NAME}.imager",
+        f"{NAME}.imager.camera",
+        f"{NAME}.imager.lepton",
+        f"{NAME}.session",
+    ],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            "config.env",
+            "sample.env",
+            ".abcli/**/*.sh",
+        ],
+    },
 )
