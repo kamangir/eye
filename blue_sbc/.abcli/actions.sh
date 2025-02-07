43e192ac831dcadf00a310ced3339112c7f79b0a
kamangir <arash@kamangir.net>
2025-02-06 21:06:06 -0800

    ++ - kamangir/bolt#746

--- blue_sbc/.abcli/actions.sh
+++ blue_sbc/.abcli/actions.sh
@@ -4,7 +4,7 @@ function blue_sbc_action_git_before_push() {
     blue_sbc build_README
     [[ $? -ne 0 ]] && return 1
 
-    [[ "$(abcli_git get_branch)" != "main" ]] &&
+    [[ "$(abcli_git get_branch)" != "current" ]] &&
         return 0
 
     blue_sbc pypi build
